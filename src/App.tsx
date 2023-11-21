4ec76497e2d640b55af262029cf341ac800f2173
Pooja Deshmukh <pooja.deshmukh@e-zest.in>
2023-11-21 22:12:36 +0530

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,38 @@
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import Home from './ui/Home';
+import Menu from './features/menu/menu';
+import Cart from './features/cart/Cart';
+import CreateOrder from './features/order/CreateOrder';
+import Order from './features/order/Order';
+
+
+
+const router =  createBrowserRouter([
+  {
+    path:'/',
+    element:<Home></Home>
+  },
+  {
+    path:'/menu',
+    element:<Menu></Menu>
+  },
+  {
+    path:'/cart',
+    element:<Cart></Cart>
+  },
+  {
+    path:'/order/new',
+    element:<CreateOrder></CreateOrder>
+  },
+  {
+    path:'/order/:orderId',
+    element:<Order></Order>
+  }
+
+])
 
 function App() {
-  return <h1>Hello vite</h1>
+  return <RouterProvider router= {router}></RouterProvider>
 }
 
 export default App
